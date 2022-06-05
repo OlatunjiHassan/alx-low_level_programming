c16ba023c64ec94735d0ba62beae6b6fba1f9fcc
Hassan <olatunjihassan22@gmail.com>
2022-06-04 18:56:22 -0700

    file4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,10 @@
 int main()
 {
   char stat[] = "\"Programming is like building a multilingual puzzle";
-
-puts(stat);
-
-return 0;
+  /**
+   * Puts functions allows me to print the value of a variable wh
+   * ich is a string
+   */
+  puts(stat);
+  return 0;
 }
