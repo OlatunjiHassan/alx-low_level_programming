f4a8b00ee9abc917566c91b8a27e00c756c01ac6
Hassan <olatunjihassan22@gmail.com>
2022-06-09 00:29:31 -0700

    file0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints "_putchar"
+ * arguments: void
+ *
+ * Return: it returns no value so void
+ */
+int main(void)
+{
+printf("_putchar");
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is the directory for all my 0x02-functions,nested loops projects
