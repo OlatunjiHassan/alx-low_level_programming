31f5bc4b7b714e4bee1eec1ac037e1289b90f918
Hassan <olatunjihassan22@gmail.com>
2022-06-16 01:36:42 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,12 +2,11 @@
 
 /**
  * main - prints "_putchar"
- * arguments: void
  *
  * Return: it returns no value so void
  */
 int main(void)
 {
-printf("_putchar");
-return (0);
+	printf("_putchar\n");
+	return (0);
 }
