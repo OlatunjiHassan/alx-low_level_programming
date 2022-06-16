e3089b182168fc987a0f3d7c236184b72bece55f
Hassan <olatunjihassan22@gmail.com>
2022-06-16 02:07:16 -0700

    put

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include "main.h"
 /**
  * main - prints "_putchar"
  *
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	putit();
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * putit -prints _putchar
+ *
+ * Return - void
+ */
+void putit(void)
+{
+	printf("_putchar\n");
+	return;
+}
