3a4eaf3dd054e586337996c454e93faf5bed5211
Hassan <olatunjihassan22@gmail.com>
2022-09-13 20:13:13 +0000

    S

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - prints all the letters of the alphabet
+ *
+ * Return: Always 0
+ */
+
+int print_alphabet(void)
+{
+	char alp = "a";
+
+	while (al <= 'z')
+	{
+		putchar(al);
+		al++;
+	}
+	putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/sample.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char vik;
+
+	for (vik = "a"; vik <= "z"; vik++)
+	{
+		putchar(vik);
+	}
+	putchar('\n');
+	return 0;
+}
+
