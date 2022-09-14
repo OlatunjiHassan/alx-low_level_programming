9d5738f8c7d32f1964ec293f5432ab765fa60f19
Hassan <olatunjihassan22@gmail.com>
2022-09-14 00:37:03 +0000

    Dd

--- 0x02-functions_nested_loops/sample.c
+++ 0x02-functions_nested_loops/sample.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include "_putchar.h"
 
 int main(void)
 {
-	int ab = 17228 % 10;
-	printf("%d", ab);
-}
+	int min1, min2, hr1, hr2;
+	hr2 = 0;
+	hr1 = 0;
+			min1 = 0;
+			while (min1 < 6)  {                                                                                                                       min2 = 0;                                                                                                               while (min2 <= 9)                                                                                                       {                                                                                                                               _putchar(hr1);
+				_putchar(hr2);
+				_putchar(':');
+				_putchar(min1);
+				_putchar(min2);
+				_putchar('\n');                                                                                                         return (0);                                                                                                             min1++;                                                                                                         }                                                                                                                       min2++;                                                                                                                 }                                                                                                               } 
+
 
