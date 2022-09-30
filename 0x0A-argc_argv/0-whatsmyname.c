cb252cbccab2b40a7c699888e6369d822264f24d
Hassan <olatunjihassan22@gmail.com>
2022-09-30 16:05:31 +0000

    mmm

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Execution starts here
+ *
+ * @argc: this  is the number of arguments on the command line
+ * @argv: this is the array of all the arguments
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This directory holds all the tasks under argc and argv, a C function which allows to manipulate inputs on the command line
